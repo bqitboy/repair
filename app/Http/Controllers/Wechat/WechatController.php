f76d15d6d7590f68bdc21e259517b6299bbe3a7f
dangran <bqitboy.qq.com>
2017-10-20 20:01:30 +0800

    1020

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -9,8 +9,35 @@ class WechatController extends Controller
     public function serve()
     {
         $app = app('wechat');
-        $app->server->setMessageHandler(function($message){
-            return "欢迎关注 Everan！";
+        $userApi = $app->user();
+        $app->server->setMessageHandler(function($message) use ($userApi) {
+            switch ($message->MsgType) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '你好：'.$userApi->get($message->FromUserName)->nickname;
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
         });
 
         return $app->server->serve();
